f53804350a1043f834fc482331e6f7ba39c0d482
alig917 <muhammadalinewforai@gmail.com>
2026-03-31 17:50:11 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/alpha-practice/" : "/",
+
   server: {
     host: "::",
     port: 8080,
